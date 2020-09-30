a0c96126ec59c543490ef43aae847503b21d7ef0
Brenzee <karelisbrendons@gmail.com>
2020-09-30 23:38:22 +0300

    Typescript

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice("yoshi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 200);
+
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+// console.log(docs);
+
+// const invOne = new Invoice("mario", "work on the mario website", 250);
+// const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+// inputs
+const type = document.querySelector("#type") as HTMLFormElement;
+const tofrom = document.querySelector("#toFrom") as HTMLFormElement;
+const details = document.querySelector("#details") as HTMLFormElement;
+const amount = document.querySelector("#amount") as HTMLFormElement;
+
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  list.render(doc, type.value, "end");
+});
